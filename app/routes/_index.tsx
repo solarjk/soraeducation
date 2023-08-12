0afa45a30faa1e1b269753909670db64a92ce78e
Junsu Kim <solarjk7@gmail.com>
2023-08-12 19:20:16 +0900

    Change BG color Change main  layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,13 +11,13 @@ export default function Index() {
   const user = useOptionalUser();
   return (
     <main className="absolute inset-0 sm:flex sm:items-center sm:justify-center">
-      <div className="absolute top-4 z-0 w-full">
+      <div className="absolute top-4 z-10 w-full">
         <NavBar />
       </div>
-      <div className="absolute inset-x-16 top-32 sm:top-64 md:inset-x-64">
-        <div className="flex flex-auto">
+      <div className="absolute inset-x-16 top-32 sm:top-64">
+        <div className="flex flex-col sm:flex-row">
           <div className="shrink basis-1/2">
-            <h3 className="text-left text-xl font-bold sm:text-2xl md:text-4xl lg:text-6xl 2xl:text-8xl ">
+            <h3 className="text-left text-2xl font-bold sm:text-2xl md:text-4xl lg:text-6xl 2xl:text-8xl ">
               <span className="block text-[#1119CB]">EXPERIENCE</span>
               <span className="block text-[#1119CB]">A REWARDING</span>
               <span className="block text-[#1119CB]">ESL JOURNEY</span>
@@ -27,7 +27,7 @@ export default function Index() {
                 English resources and tutoring!
               </p>
               <button
-                className="rounded-full bg-[#111E90] px-16 py-2 text-xs font-semibold text-white sm:text-lg"
+                className="mt-16 h-12 w-48 rounded-full bg-[#111E90] text-xs font-semibold text-white sm:text-lg"
                 onClick={() => navigate("/pricing")}
               >
                 Find out more
@@ -35,7 +35,7 @@ export default function Index() {
               <link rel="icon" href="localhost:3000/favicon.ico?v=2" />
             </h3>
           </div>
-          <div className="shrink basis-1/2">
+          <div className="mt-8 shrink basis-1/2 sm:mt-0">
             <img
               className="ml-auto"
               src="home_students.png"
